9261d51715d02e1e9840fadb6a1bbf06cc470ccd
James Vanderhyde <jvanderhyde@benedictine.edu>
2014-12-23 16:29:56 -0600

    Created NetBeans project

---
+++ java/TiniestRPG/src/us/vanderhyde/TiniestRPG.java
@@ -0,0 +1,17 @@
+//Command-line java version of the World's Tiniest RPG
+//Created by James Vanderhyde, 23 December 2014
+
+package us.vanderhyde;
+
+/**
+ * Command-line application for the World's Tiniest RPG.
+ * @author James Vanderhyde
+ */
+public class TiniestRPG
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
